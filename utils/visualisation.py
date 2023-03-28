691c1a72d4fccc573d15a29446e53f2388b84f02
ilyaskalimullinn <ilyaskalimullinn@yandex.ru>
2023-03-28 13:10:08 +0300

    [add] simple visualisation

--- utils/visualisation.py
+++ utils/visualisation.py
@@ -1,6 +1,53 @@
 import plotly.graph_objects as go
 import numpy as np
 
-class Visualisation():
 
-    pass
+class Visualisation:
+    def plot_accuracy(self, accuracy_valid: np.ndarray, accuracy_train: np.ndarray, number_of_epochs: int, plot_title: str = "Accuracy of model"):
+        fig = go.Figure()
+
+        epochs = np.arange(1, number_of_epochs + 1)
+
+        fig.add_trace(go.Scatter(
+            x=epochs,
+            y=accuracy_valid,
+            mode='lines',
+            name='Accuracy on validation set',
+        ))
+
+        fig.add_trace(go.Scatter(
+            x=epochs,
+            y=accuracy_train,
+            mode='lines',
+            name='Accuracy on training set',
+        ))
+
+        fig.update_layout(
+            title=plot_title,
+            xaxis_title="Epochs",
+            yaxis_title="Accuracy",
+            legend_title="Legend",
+        )
+
+        fig.show()
+
+    def plot_target_function(self, target_func_values: np.ndarray, number_of_epochs: int, plot_title: str = "Target function values on train set, ln"):
+        fig = go.Figure()
+
+        epochs = np.arange(1, number_of_epochs + 1)
+
+        fig.add_trace(go.Scatter(
+            x=epochs,
+            y=np.log(target_func_values),
+            mode='lines',
+            name='Target function values on training set, ln',
+        ))
+
+        fig.update_layout(
+            title=plot_title,
+            xaxis_title="Epochs",
+            yaxis_title="Target function value, ln",
+            legend_title="Legend",
+        )
+
+        fig.show()
