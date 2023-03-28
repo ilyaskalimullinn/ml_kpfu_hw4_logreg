20d5218e99b21ff93d8225967ad6cf24ec5bf00f
ilyaskalimullinn <ilyaskalimullinn@yandex.ru>
2023-03-28 16:53:32 +0300

    [add] main task (experiment)

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-# This is a sample Python script.
+import numpy as np
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+from config.logistic_regression_config import cfg
+from datasets.digits_dataset import Digits
+from models.logistic_regression_model import LogReg
+from utils.metrics import accuracy, confusion_matrix
+from utils.visualisation import Visualisation
 
 
-# Press the green button in the gutter to run the script.
+def main_task():
+    digits = Digits(cfg)
+    logreg = LogReg(cfg, digits.k, digits.d)
+    logreg.train(digits.inputs_train, digits.targets_train, digits.inputs_valid, digits.targets_valid)
+
+    predictions = logreg(digits.inputs_test)
+
+    test_accuracy = accuracy(predictions, digits.targets_test)
+    test_confusion_matrix = confusion_matrix(predictions, digits.targets_test, digits.k)
+
+    print(f"Test accuracy: {test_accuracy}, confusion matrix: ")
+    print(test_confusion_matrix)
+
+    visualisation = Visualisation()
+    visualisation.plot_accuracy(np.array(logreg.accuracy_valid), np.array(logreg.accuracy_train), cfg.nb_epoch,
+                                f"Accuracy on train set and validation set; testing accuracy is {round(test_accuracy, 2)}")
+    visualisation.plot_target_function(np.array(logreg.target_func_values), cfg.nb_epoch)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main_task()
