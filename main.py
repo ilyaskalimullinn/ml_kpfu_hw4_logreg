7043e0c4108a03a4117997d9d81380e488689c3a
ilyaskalimullinn <ilyaskalimullinn@yandex.ru>
2023-04-04 16:22:29 +0300

    [add] model saving using pickle

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import pickle
+
 import numpy as np
 import os
 
@@ -8,10 +10,7 @@ from utils.metrics import accuracy, confusion_matrix
 from utils.visualisation import Visualisation
 
 
-def main_task():
-    digits = Digits(cfg)
-    logreg = LogReg(cfg, digits.k, digits.d, reg_coeff=0.005)
-    logreg.train(digits.inputs_train, digits.targets_train, digits.inputs_valid, digits.targets_valid)
+def main_task(logreg: LogReg, digits: Digits):
 
     predictions = logreg(digits.inputs_test)
 
@@ -29,7 +28,32 @@ def main_task():
                                        save_path=os.path.join(ROOT_DIR, 'graphs/target_function.html'))
 
 
+def save_model(logreg: LogReg, path: str):
+    with open(path, 'wb+') as f:
+        pickle.dump(logreg, f)
+
+
+def load_model(path: str):
+    with open(path, 'rb') as f:
+        return pickle.load(f)
+
+
 if __name__ == '__main__':
     ROOT_DIR = os.path.abspath(os.curdir)
     np.random.seed(200)
-    main_task()
+    digits = Digits(cfg)
+
+    model_path = input("Do you want to get a model from a file? Please, specify the file name or leave it empty: ").strip()
+    if len(model_path) != 0:
+        model_path = os.path.join(ROOT_DIR, "saves", model_path)
+        logreg = load_model(model_path)
+    else:
+        logreg = LogReg(cfg, digits.k, digits.d, reg_coeff=0.005)
+        logreg.train(digits.inputs_train, digits.targets_train, digits.inputs_valid, digits.targets_valid)
+
+    main_task(logreg, digits)
+
+    filename = input("Save model's params in the file? Enter the file name (leave empty if you don't want to save): ").strip()
+    if len(filename) != 0:
+        path = os.path.join(ROOT_DIR, "saves", filename)
+        save_model(logreg, path)
